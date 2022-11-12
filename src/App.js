9033fdb839a9923db1ba3ed274d25f124201b3bc
Bensarras <benjamin.sarras@gmail.com>
2022-11-12 18:12:23 -0300

    labtest10
    w

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import './Form.css';
+import Personal from './Personal';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <div class="main-block">
+    // <form action="/">
+    //   <h1>LAB WEEK 10</h1>
+      
+    //   <fieldset>
+    //     <legend>
+    //       <h3>Personal Details</h3>
+    //     </legend>
+    //     <div  class="personal-details">
+    //       <div>
+    //         <div><label>Name*</label><input type="text" name="name" required></input></div>
+    //         <div><label>Email*</label><input type="text" name="name" required></input></div>
+    //         <div><label>Phone*</label><input type="text" name="name" required></input></div>
+    //         <div><label>Street</label><input type="text" name="name"></input></div>
+    //         <div><label>City*</label><input type="text" name="name" required></input></div>
+    //         <label>Province*</label>  
+    //           <select>
+    //             <option value=""></option>
+    //             <option value="Ontario">Ontario</option>
+    //             <option value="Quebec">Quebec</option>
+    //             <option value="Alberta">Alberta</option>
+    //             <option value="Nova scotia">Nova scotia</option>
+                
+    //           </select>
+    //           <div><label>Postal code*</label><input type="text" name="name" required></input></div>
+            
+
+              
+          
+    //       </div>
+    //     </div>
+    //   </fieldset>
+      
+    //   <button type="submit" href="/">Submit</button>
+    // </form>
+    // </div> 
+    // </div>
+    <Personal></Personal>
   );
 }
 
---
+++ src/Personal.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import './Form.css';
+
+const INITIAL_VALUE = {
+    fullname: "",
+    email: "",
+    address1: "",
+    address2: "",
+    city: "",
+    province: "",
+    postalCode: "",
+}
+// const personalinfo = {
+//     fullname: "Full Name",
+//     email: "Email",
+//     address1: "Address 1",
+//     address2: "Address 2",
+//     city: "City",
+//     province: "Province",
+//     postalCode: "Postal Code",
+// }
+
+
+export default class Personal extends React.Component{
+    constructor(props){
+        super(props)
+        
+        this.state = {...INITIAL_VALUE}
+        this.cities = ["[--SELECT CITY--]", "Toronto", "Quebec", "Vancuber", "Calgary", "Ottawa"]
+        this.province = ["[--SELECT province--]", "Ontario", "Quebec", "Alberta", "Nova scotia"]
+    }
+
+    onSubmitLogin = (event) => {
+        event.preventDefault()
+        INITIAL_VALUE.fullname = this.state.fullname
+        INITIAL_VALUE.email = this.state.email
+        INITIAL_VALUE.address1 = this.state.address1
+        INITIAL_VALUE.address2 = this.state.address2
+        INITIAL_VALUE.city = this.state.city
+        INITIAL_VALUE.province = this.state.province
+        INITIAL_VALUE.postalCode= this.state.postalCode
+        
+        console.log(INITIAL_VALUE)
+    
+
+        //console.log(event.target)
+    }
+
+    clearForm = (event) => {
+        event.preventDefault()
+        this.setState({...INITIAL_VALUE})
+    }
+
+    onValueChanged = (event) => {
+        ///console.log(event.target.value)
+
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    render= () => {
+        return(
+            <>
+                <h1>Personal info FORM Lab10</h1>
+                <form>
+                    <input 
+                        type="text" 
+                        name="fullname" 
+                        placeholder="Enter Fullname"
+                        value={this.state.fullname}
+                        onChange={event => this.onValueChanged(event)} />
+                    <input 
+                        type="text" 
+                        name="email" 
+                        placeholder="Enter email"
+                        value={this.state.email} 
+                        onChange={event => this.onValueChanged(event)} />
+                        <input 
+                        type="text" 
+                        name="address1" 
+                        placeholder="Enter address"
+                        value={this.state.address1}
+                        onChange={event => this.onValueChanged(event)} />
+                        <input 
+                        type="text" 
+                        name="address2"
+                        placeholder="Enter address"
+                        value={this.state.address2}
+                        onChange={event => this.onValueChanged(event)} />
+                    <select onChange={this.onValueChanged} name="city">
+                        {
+                            this.cities.map(city => (
+                                <option key={city} value={city}>{city}</option>
+                            ))
+                        }
+                    </select>
+                    <select onChange={this.onValueChanged} name="province">
+                        {
+                            this.province.map(province => (
+                                <option key={province} value={province}>{province}</option>
+                            ))
+                        }
+                    </select>
+                    <input 
+                        type="text" 
+                        name="postalCode" 
+                        placeholder="Enter Postal Code"
+                        value={this.state.postalCode}
+                        onChange={event => this.onValueChanged(event)} />
+                    <input 
+                        type="submit" 
+                        name="btnSubmit" 
+                        value="Sumbit form" 
+                        onClick={this.onSubmitLogin} />
+                        <div>
+                    <h1>email : {INITIAL_VALUE.email}</h1>
+                    <h1>fullname : {INITIAL_VALUE.fullname}</h1>
+                    <h1>address : {INITIAL_VALUE.address1}</h1>
+                    <h1>address2 : {INITIAL_VALUE.address2}</h1>
+                    <h1>city: {INITIAL_VALUE.city}</h1>
+                    <h1>province : {INITIAL_VALUE.province}</h1>
+                    <h1>postalCode : {INITIAL_VALUE.postalCode}</h1>
+                    </div>
+                </form>
+                
+                
+                
+            </>
+        )
+    }
+}
